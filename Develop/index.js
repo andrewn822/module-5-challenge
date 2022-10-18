51e55bad5b2852b255ab598de05c1905092ee41e
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-10-18 15:24:06 -0700

    commit changes

--- Develop/index.js
+++ Develop/index.js
@@ -9,7 +9,46 @@ $('saveBtn').on('click', function() {
 })
 
 function displayTime() {
-    var today = moment().format('MMMM Do YYYY, h:mm:ss a')
+    var today = moment().format('MMMM Do YYYY, [at] h:mm:ss a')
     timeDisplay.text(today)
 }
 
+setInterval(displayTime, 1000)
+
+var currentHour = moment().hour()
+
+$('.time-block').each(function(){
+    var blockHour = parseInt($(this).attr('id').split('hour')[1]);
+   
+    if (currentHour > blockHour){
+        $(this).addClass('past')
+        $(this).removeClass('present')
+        $(this).removeClass('future');
+    }
+
+    else if(currentHour < blockHour){
+        $(this).addClass('future')
+        $(this).removeClass('present')
+        $(this).removeClass('past'); 
+    }
+
+    else if (currentHour === blockHour){
+        $(this).removeClass('past')
+        $(this).removeClass('future')
+        $(this).addClass('present')
+    }
+})
+
+$(document).ready(function(){
+    $('#hour8 textarea').val(localStorage.getItem('hour8'))
+    $('#hour9 textarea').val(localStorage.getItem('hour9'))
+    $('#hour10 textarea').val(localStorage.getItem('hour10'))
+    $('#hour11 textarea').val(localStorage.getItem('hour11'))
+    $('#hour12 textarea').val(localStorage.getItem('hour12'))
+    $('#hour13 textarea').val(localStorage.getItem('hour13'))
+    $('#hour14 textarea').val(localStorage.getItem('hour14'))
+    $('#hour15 textarea').val(localStorage.getItem('hour15'))
+    $('#hour16 textarea').val(localStorage.getItem('hour16'))
+    $('#hour17 textarea').val(localStorage.getItem('hour17'))
+
+})
